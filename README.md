eb9f03960b7b7e6080529f3d2ca7ac04e11d3f6f
adam-varga <adam@vrg.hu>
2021-01-28 11:38:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-3bw1sz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-3bw1sz)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>I am Adam, from the browser</p>
+    </div>
+  );
+}
